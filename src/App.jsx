2f179a3ba8079b04254769b1edc9f5df5e88fd52
Nithin-1904 <nithinsarode7@gmail.com>
2024-03-14 15:47:20 +0530

    integration with react-hook-form

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
 import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <>
       <div id="form">
         <h1>Registration Form</h1>
         <h4>Fill the below details</h4>
-        <form
-          onSubmit={() => {
-            console.log(document.getElementById("fname"));
-            console.log(document.getElementById("lname"));
-            console.log(document.getElementById("mail"));
-            console.log(document.getElementById("phno"));
-            console.log(document.getElementById("dob"));
-          }}
-        >
+        <form onSubmit={handleSubmit(onSubmit)}>
           <label>First Name: &nbsp;</label>
           <br></br>
-          <input type="text" id="fname" name="fname" placeholder="FirstName" />
+          <input {...register("fname")} id="fname" placeholder="FirstName" />
+
           <label>Last Name: &nbsp;</label>
           <br></br>
-          <input type="text" id="lname" name="lname" placeholder="LastName" />
+          <input {...register("lname")} id="lname" placeholder="LastName" />
+
           <label>Email: &nbsp;</label>
           <br></br>
-          <input type="email" id="mail" name="mail" placeholder="E-mail" />
+          <input
+            {...register("mail", { pattern: /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/ })}
+            id="mail"
+            placeholder="E-mail"
+          />
+
           <label>Conact Number: &nbsp;</label>
           <br></br>
-          <input type="text" id="phno" name="phno" placeholder="Phone number" />
+          <input
+            {...register("phno", { valueAsNumber: true })}
+            id="phno"
+            placeholder="Phone number"
+          />
           <label>Date Of Birth: &nbsp;</label>
           <br></br>
-          <input type="date" id="dob" name="dob" placeholder="Date of Birth" />
+          <input
+            {...register("dob")}
+            id="dob"
+            type="date"
+            placeholder="ddmmyyyy"
+          />
           <input type="submit" id="submit" placeholder="Submit" />
         </form>
       </div>
